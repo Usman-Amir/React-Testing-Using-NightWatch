955c40f2a745834b63930744baa77a1941a8ba84
UsmanAmir <muhammad.usman.amir@exact.com>
2018-03-13 15:53:04 +0800

    Initial Commit

---
+++ pages/loginPage.js
@@ -0,0 +1,31 @@
+
+import React from 'React.js';
+const loginCommands = {
+  login(email, pass) {
+    return this
+      .waitForElementVisible('@emailInput')
+      .setValue('@emailInput', email)
+      .setValue('@passInput', pass)
+      .waitForElementVisible('@loginButton')
+      .click('@loginButton')
+  }
+};
+
+
+const result =  [{
+  url: 'https://dashboard.syncano.io/#/login',
+  commands: [loginCommands],
+  elements: {
+    emailInput: {
+      selector: 'input[type=text]'
+    },
+    passInput: {
+      selector: 'input[name=password]'
+    },
+    loginButton: {
+      selector: 'button[type=submit]'
+    }
+  }
+}];
+
+export default result;
