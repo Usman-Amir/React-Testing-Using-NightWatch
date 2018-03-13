5843d185fe1d597b7df1fe424468c8d14be609cf
Amir Ali Jiwani <amirali.jiwani89@gmail.com>
2018-03-13 17:23:30 +0800

    Adds gitignore and other utilities to project

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -1,31 +1,29 @@
-
-import React from 'React.js';
 const loginCommands = {
-  login(email, pass) {
-    return this
-      .waitForElementVisible('@emailInput')
-      .setValue('@emailInput', email)
-      .setValue('@passInput', pass)
-      .waitForElementVisible('@loginButton')
-      .click('@loginButton')
-  }
+	login(email, pass) {
+		return this.waitForElementVisible("@emailInput")
+			.setValue("@emailInput", email)
+			.setValue("@passInput", pass)
+			.waitForElementVisible("@loginButton")
+			.click("@loginButton");
+	},
 };
 
-
-const result =  [{
-  url: 'https://dashboard.syncano.io/#/login',
-  commands: [loginCommands],
-  elements: {
-    emailInput: {
-      selector: 'input[type=text]'
-    },
-    passInput: {
-      selector: 'input[name=password]'
-    },
-    loginButton: {
-      selector: 'button[type=submit]'
-    }
-  }
-}];
+const result = [
+	{
+		url: "https://dashboard.syncano.io/#/login",
+		commands: [loginCommands],
+		elements: {
+			emailInput: {
+				selector: "input[type=text]",
+			},
+			passInput: {
+				selector: "input[name=password]",
+			},
+			loginButton: {
+				selector: "button[type=submit]",
+			},
+		},
+	},
+];
 
 export default result;
